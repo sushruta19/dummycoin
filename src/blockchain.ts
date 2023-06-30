d03b8301688240bb64f8e7b1c8405286c5425f62
sushruta19 <sensoubhik2001@gmail.com>
2023-06-30 11:02:28 +0530

    Implement proof of work for block mining
    Modified the `Block` class to include a `nonce` property and a `mineBlock` method.
    The `mineBlock` method now iteratively calculates the hash of the block by
    incrementing the `nonce` value until the resulting hash satisfies the specified
    difficulty level.
    
    The `mineBlock` method now ensures that computational effort is required to add
    new blocks to the chain, enhancing the security and integrity of the blockchain.
    
    Changes Made:
    - Added `nonce` property to the `Block` class
    - Modified `calculateHash` method in the `Block` class to include the `nonce`
    - Implemented `mineBlock` method in the `Block` class to perform proof of work
    - Added `difficulty` property to the `Blockchain` class
    - Adjusted block mining process in the `addBlock` method of the `Blockchain` class
      to use the specified difficulty level
    - Updated the creation of the genesis block to incorporate proof of work
    
    Tested the implementation by adding new blocks, verifying the chain's validity,
    and displaying the blockchain in the console.
    
    Impact:
    - Increased security and integrity of the blockchain by requiring computational
      effort (proof of work) for block mining
    - Improved resistance against tampering and potential attacks

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -6,36 +6,52 @@ class Block {
   private data : any;
   public previousHash : string;
   public hash : string;
+  public nonce : number;
 
   constructor(index:number, timestamp:string, data:any, previousHash:string='') {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
+    this.nonce = 0;
     this.hash = this.calculateHash();
   }
 
   calculateHash() : string {
     return SHA256(
-      this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash
+      this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty : number) : void {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty).fill("0").join("")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block Mined: " + this.hash);
   }
 }
 
 class Blockchain {
   chain : Block[];
+  difficulty : number;
 
   constructor() {
+    this.difficulty = 3;
     this.chain = [this.createGenesisBlock()];
   }
 
-  createGenesisBlock() : Block {
-    return new Block(0, "20/06/2023", "Genesis Block", "0");
+  private createGenesisBlock() : Block {
+    let genesisBlock : Block = new Block(0, "20/06/2023", "Genesis Block", "0");
+    genesisBlock.mineBlock(this.difficulty);
+    return genesisBlock;
   }
 
   addBlock(newBlock : Block) : void {
     newBlock.previousHash = this.getLatestBlock().hash;
     newBlock.hash = newBlock.calculateHash();
+
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -71,7 +87,10 @@ class Blockchain {
 let dummycoin = new Blockchain();
 
 //adding new blocks
+console.log("Mining Block 1...");
 dummycoin.addBlock(new Block(1, "21/06/2023", {amount : 4}));
+
+console.log("Mining Block 2...");
 dummycoin.addBlock(new Block(2, "23/06/2023", {amount : 2}));
 
 //displaying the entire blockchain in console
@@ -80,10 +99,10 @@ console.log(JSON.stringify(dummycoin, null, 1));
 //checking whether chain has been tempered or not
 console.log("Is chain valid?", dummycoin.isChainValid());
 
-//tampering the chain
-//changed the hash of second block
-dummycoin.chain[1].hash = "bulbul";
-console.log("Is chain valid?", dummycoin.isChainValid());
+//tampering the chain by
+//changing the hash of second block
+// dummycoin.chain[1].hash = "bulbul";
+// console.log("Is chain valid?", dummycoin.isChainValid());
 
 //body, timestamp, index has been set private and cannot be changed from outside
 //even if someone try to change the body, and recalculate hash of the block,
