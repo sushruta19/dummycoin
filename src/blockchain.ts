b506e3faac3afa2a21c6c0085ada7956faceaf4b
sushruta19 <sensoubhik2001@gmail.com>
2023-06-30 17:09:19 +0530

    feat: Implement pending transaction and mining reward in Blockchain class
    - Created a Transaction class to represent transaction data in a block
    - Modified Blockchain class to use a pending transaction list
      before mining
    - Introduced a mining reward for miners in the form of coins
    - Implemented a getBalanceAddress method to calculate wallet
      balances
    - Added difficulty level adjustment every 10 blocks mined
    - Improved block mining and hashing logic
    
    This commit introduces several new features to the Blockchain
    class, including transaction handling, mining rewards, balance
    calculation, difficulty level adjustment, and improved block
    mining and hashing. These changes improve the functionality and
    security of the blockchain system.

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,17 +1,27 @@
 const { SHA256 } = require("crypto-js")
 
+class Transaction {
+  fromAddress : string;
+  toAddress : string;
+  amount : number;
+
+  constructor(fromAddress : string, toAddress : string, amount : number) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;           
+  }
+}
+
 class Block {
-  private index : number;
-  private timestamp : string;
-  private data : any;
+  private timestamp : number;
+  private transactions : Transaction[];
   public previousHash : string;
   public hash : string;
   public nonce : number;
 
-  constructor(index:number, timestamp:string, data:any, previousHash:string='') {
-    this.index = index;
+  constructor(timestamp:number, transactions:Transaction[], previousHash:string='') {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.nonce = 0;
     this.hash = this.calculateHash();
@@ -19,7 +29,7 @@ class Block {
 
   calculateHash() : string {
     return SHA256(
-      this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce
+      this.timestamp + JSON.stringify(this.transactions) + this.previousHash + this.nonce
     ).toString();
   }
 
@@ -30,36 +40,86 @@ class Block {
     }
     console.log("Block Mined: " + this.hash);
   }
+
+  getTransactionsData() : Transaction[] {
+    return this.transactions;
+  }
 }
 
 class Blockchain {
-  chain : Block[];
-  difficulty : number;
+  private chain : Block[];
+  private difficulty : number;
+  private pendingTransactions : Transaction[];
+  private miningReward : number;
+  private lastDifficultyAdjustmentBlock : number;
 
   constructor() {
     this.difficulty = 3;
     this.chain = [this.createGenesisBlock()];
+    this.pendingTransactions = [];
+    this.miningReward = 50;
+    this.lastDifficultyAdjustmentBlock = 0;
   }
 
   private createGenesisBlock() : Block {
-    let genesisBlock : Block = new Block(0, "20/06/2023", "Genesis Block", "0");
+    let genesisBlock : Block = new Block(Date.now(), [{fromAddress:"null", toAddress:"null", amount:0}], "0");
+    console.log("Mining Genesis Block...");
     genesisBlock.mineBlock(this.difficulty);
     return genesisBlock;
   }
 
-  addBlock(newBlock : Block) : void {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
-
-    newBlock.mineBlock(this.difficulty);
-    this.chain.push(newBlock);
+  private getDifficulty() : number {
+    const blocksPerDifficultyAdjustment = 10;
+    if(this.chain.length - this.lastDifficultyAdjustmentBlock >= blocksPerDifficultyAdjustment) {
+      this.difficulty ++;
+      this.lastDifficultyAdjustmentBlock = this.chain.length;
+    }
+    return this.difficulty;
   }
 
-  getLatestBlock() : Block {
+  public minePendingTransactions(miningRewardAddress : string) : void {
+    const rewardTransaction : Transaction = new Transaction(
+      "null", miningRewardAddress, this.miningReward);
+
+    this.pendingTransactions.push(rewardTransaction);
+
+    let newBlock : Block = new Block(
+      Date.now(), this.pendingTransactions, this.chain[this.chain.length-1].hash
+    ); 
+
+    newBlock.mineBlock(this.getDifficulty());
+    this.chain.push(newBlock);
+
+    this.pendingTransactions = [];
+  }
+
+  public createTransaction(transaction : Transaction) : void {
+    this.pendingTransactions.push(transaction);
+  }
+
+  public getBalanceAddress(walletAddress : string) : number{
+    let balance = 0;
+    
+    for(let i = 0; i<this.chain.length; i++) {
+      const transactionList = this.chain[i].getTransactionsData();
+      for(let j = 0; j<transactionList.length; j++) {
+        const unitTransaction = transactionList[j];
+        if(unitTransaction.fromAddress === walletAddress) {
+          balance -= unitTransaction.amount;
+        }
+        if(unitTransaction.toAddress === walletAddress) {
+          balance += unitTransaction.amount;
+        }
+      }
+    }
+    return balance;
+  }
+
+  public getLatestBlock() : Block {
     return this.chain[this.chain.length-1];
   }
   
-  isChainValid() : boolean {
+  public isChainValid() : boolean {
     let previousBlock : Block = this.chain[0];
 
     if(previousBlock.hash !== previousBlock.calculateHash()) {
@@ -86,26 +146,38 @@ class Blockchain {
 // instantiating a blockchain
 let dummycoin = new Blockchain();
 
-//adding new blocks
-console.log("Mining Block 1...");
-dummycoin.addBlock(new Block(1, "21/06/2023", {amount : 4}));
+dummycoin.createTransaction(new Transaction("address1", "address2", 100));
+dummycoin.createTransaction(new Transaction("address2", "address1", 50));
 
-console.log("Mining Block 2...");
-dummycoin.addBlock(new Block(2, "23/06/2023", {amount : 2}));
+console.log("\nStarting the miner...");
+dummycoin.minePendingTransactions("address3");
 
-//displaying the entire blockchain in console
-console.log(JSON.stringify(dummycoin, null, 1));
+console.log("Balance of address3 is " + dummycoin.getBalanceAddress("address3"));
+
+dummycoin.createTransaction(new Transaction("address4", "address3", 100));
+dummycoin.createTransaction(new Transaction("address3", "address1", 50));
+
+console.log("\nStarting the miner...");
+dummycoin.minePendingTransactions("address3");
+
+console.log("Balance of address3 is " + dummycoin.getBalanceAddress("address3"));
 
 //checking whether chain has been tempered or not
-console.log("Is chain valid?", dummycoin.isChainValid());
+console.log("\nIs chain valid?", dummycoin.isChainValid());
 
-//tampering the chain by
-//changing the hash of second block
+//seeing the latest block
+console.log("\nLatest Block :\n", dummycoin.getLatestBlock());
+
+//displaying the entire blockchain in console
+// console.log(JSON.stringify(dummycoin, null, 1));
+
+// tampering the chain by
+// changing the hash of second block
 // dummycoin.chain[1].hash = "bulbul";
 // console.log("Is chain valid?", dummycoin.isChainValid());
 
-//body, timestamp, index has been set private and cannot be changed from outside
-//even if someone try to change the body, and recalculate hash of the block,
-//it will still be caught since its correct hash is stored in the successive block
-//the only loophole is that the lastest block can be tampered if the body and corresponding
-//hash is changed since its correct hash is stored nowhere as there is no successive block
+// data(transactions), timestamp, index has been set private and cannot be changed from outside
+// even if someone try to change the transaction data, and recalculate hash of the block,
+// it will still be caught since its correct hash is stored in the successive block
+// the only loophole is that the lastest block can be tampered if the its transaction data
+// and corresponding hash is changed since its correct hash is stored nowhere as there is no successive block
